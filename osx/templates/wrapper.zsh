1bcdaf3300fb31af02e89e70f16d403fc453cb7d
Nash Spence <nashspence@gmail.com>
2025-09-01 17:23:47 -0700

    Add release-only wrapper template
    * Add release-only wrapper template
    
    * Update rclone version to latest in release.yaml

--- osx/templates/wrapper.zsh
+++ osx/templates/wrapper.zsh
@@ -2,4 +2,4 @@
 set -euo pipefail
 # Auto-generated wrapper. Invokes 'run-podman-script' against a specific build file.
 export DR_WRAPPER_NAME="%NAME%"
-exec run-podman-script "%ABS%" "$@"
+exec run-podman-script -f "%ABS%" "$@"
