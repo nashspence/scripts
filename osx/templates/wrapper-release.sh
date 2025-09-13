dec05159d7206caf9d0e7769c41843276fceb431
Nash Spence <nashspence@gmail.com>
2025-09-13 07:44:52 -0700

    Make use-scripts-machine foreground and poll

--- osx/templates/wrapper-release.sh
+++ osx/templates/wrapper-release.sh
@@ -2,6 +2,6 @@
 set -eu
 # Auto-generated wrapper. Runs a portable container image.
 export DR_WRAPPER_NAME="%NAME%"
-use-scripts-machine "$$" &
+use-scripts-machine "$$"
 img=$(nsimg "%NAME%")
 exec podman run --rm "$img" "$@"
